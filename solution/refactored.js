8360d142188e683797761a0397ca73e6cb0199de
zenby <zenbysel@gmail.com>
2025-03-01 00:08:55 +0100

    feat: procedure refactoring

---
+++ solution/refactored.js
@@ -0,0 +1,33 @@
+function printTable(data) {
+  if (!data) return;
+
+  const table = [];
+  let max = 0;
+
+  const lines = data.split('\n').slice(1, -1);
+  for (const line of lines) {
+    const [city, population, area, density, country] = line.split(',');
+    const d = parseInt(density);
+    max = Math.max(max, d);
+    table.push([city, population, area, density, country]);
+  }
+
+  for (const row of table) {
+    const a = Math.round((row[3] * 100) / max);
+    row.push(a.toString());
+  }
+
+  table.sort((r1, r2) => r2[5] - r1[5]);
+
+  for (const row of table) {
+    let s = row[0].padEnd(18);
+    s += row[1].padStart(10);
+    s += row[2].padStart(8);
+    s += row[3].padStart(8);
+    s += row[4].padStart(18);
+    s += row[5].padStart(6);
+    console.log(s);
+  }
+}
+
+export default printTable;
---
+++ solution/refactored.test.js
@@ -0,0 +1,79 @@
+import { expect, test, describe, vi, afterAll } from 'vitest';
+import printTable from './refactored.js';
+
+const data = `city,population,area,density,country
+Shanghai,24256800,6340,3826,China
+Delhi,16787941,1484,11313,India
+Lagos,16060303,1171,13712,Nigeria
+Istanbul,14160467,5461,2593,Turkey
+Tokyo,13513734,2191,6168,Japan
+Sao Paulo,12038175,1521,7914,Brazil
+Mexico City,8874724,1486,5974,Mexico
+London,8673713,1572,5431,United Kingdom
+New York City,8537673,784,10892,United States
+Bangkok,8280925,1569,5279,Thailand`;
+
+describe('printTable', () => {
+  const consoleMock = vi.spyOn(console, 'log');
+
+  afterAll(() => {
+    consoleMock.mockReset();
+  });
+
+  test('prints nothing for only header passed', () => {
+    printTable(`city,population,area,density,country`);
+
+    expect(consoleMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('omits last item in data passed', () => {
+    printTable(`city,population,area,density,country
+      Bangkok,8280925,1569,5279,Thailand`);
+
+    expect(consoleMock).toHaveBeenCalledTimes(0);
+  });
+
+  test('print example table', () => {
+    printTable(data);
+
+    expect(consoleMock).toHaveBeenCalledTimes(9);
+    expect(consoleMock).not.toHaveBeenCalledWith(expect.stringContaining('Bangkok'));
+
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      1,
+      'Lagos               16060303    1171   13712           Nigeria   100'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      2,
+      'Delhi               16787941    1484   11313             India    83'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      3,
+      'New York City        8537673     784   10892     United States    79'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      4,
+      'Sao Paulo           12038175    1521    7914            Brazil    58'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      5,
+      'Tokyo               13513734    2191    6168             Japan    45'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      6,
+      'Mexico City          8874724    1486    5974            Mexico    44'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      7,
+      'London               8673713    1572    5431    United Kingdom    40'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      8,
+      'Shanghai            24256800    6340    3826             China    28'
+    );
+    expect(consoleMock).toHaveBeenNthCalledWith(
+      9,
+      'Istanbul            14160467    5461    2593            Turkey    19'
+    );
+  });
+});
